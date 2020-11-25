d9ad80030c490055bcc578a1b449247fba152c39
nYalla426 <75041930+nYalla426@users.noreply.github.com>
2020-11-25 18:50:52 -0500

    Update rock.py

--- rock.py
+++ rock.py
@@ -16,24 +16,7 @@ class Rock:
         
         return False
     '''
-    honestly don't know what I was tryna do, proabably talk about
-    prehistoric periods, but eh delete it if you can't salvage it
     
-    def period(self):
-        if self.age
-    
-    def subType(self):
-        if self.type.casefold() == 'igneous':
-            print("Extrusive or Intrusive rocks")
-                
-        elif self.type.casefold() == 'sedimentary':
-            print('Organic, Clastic, Chemical')
-            
-        elif self.type.casefold() == 'metamorphic':
-            print('Foliated and Unfoliated')
-        else:
-            print('Unfortunately, there is no subtype found')
-    '''
 class Igneous(Rock):
     def __init__(self, age=None):
         super.__init__('igneous', age)
